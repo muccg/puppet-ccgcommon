4a895437d2b597572ade3432e62fcdde5e6dc599
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2015-06-30 22:10:34 +0800

    1.5.9 remove clunky fail2ban setup

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,13 +11,6 @@ class ccgcommon (
     timezone => $timezone,
   }
 
-  class { 'fail2ban':
-    jails_config   => 'file',
-    jails_template => 'fail2ban/jail.local.erb',
-    jails          => ['ssh', ],
-    require        => Class['timezone'],
-  }
-
   # $ssh_user is a fact injected by ccgplatform
   # under ccgplatform the workflow requires a working dir
   # for $ssh_user
